af8834649a7e1e3bc881ddd95d4718cecc14c3c2
CyberXpert <nurainiabubakar3537@gmail.com>
2026-01-14 14:30:18 +0100

    now can be run as a script for ease

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from v1.api import router as router_v1
 from v2.api import router as router_v2
@@ -14,3 +15,6 @@ app.add_middleware(
 
 app.include_router(router_v1)
 app.include_router(router_v2)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
