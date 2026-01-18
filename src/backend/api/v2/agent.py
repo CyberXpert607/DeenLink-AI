26a049da7437e573e3cb36a2f387c5cbb92c1938
CyberXpert <nurainiabubakar3537@gmail.com>
2026-01-18 22:51:12 +0100

    now supports the new added knowledge base and updated output format

--- src/backend/api/v2/agent.py
+++ src/backend/api/v2/agent.py
@@ -18,31 +18,40 @@ def generate_chat_response(user_question: str) -> str:
 
 def generate_rag_answer(question: str, retrieved_docs: list):
     retrieved_docs = retrieved_docs[:3]
+
     if not retrieved_docs:
         return {
-            "answer": (
-                "I could not find any authentic sources that directly answer this question."
-            ),
+            "answer_html": "I could not find any authentic sources that directly answer this question.",
             "sources": []
         }
 
-    sources_text = "\n\n".join(
-        f"""
-        Collection: {doc['collection']}
-        Narrator: {doc.get('narrator', 'Unknown')}
-        Arabic:  {doc['arabic']}
-        English:  {doc['english']}
-        """
+    sources_blocks = []
 
-        for doc in retrieved_docs
-    )
+    for doc in retrieved_docs:
+        if doc["source_type"] == "hadith":
+            sources_blocks.append(f"""
+            Collection: {doc['collection']}
+            Narrator: {doc.get('narrator', 'Unknown')}
+            Arabic: {doc['arabic']}
+            English: {doc['english']}
+            """)
+
+        elif doc["source_type"] == "quran":
+            sources_blocks.append(f"""
+            Qur'an
+            Surah: {doc['surah_name']}
+            Ayah: {doc['ayah']}
+            Arabic: {doc['arabic']}
+            English: {doc['english']}
+            """)
+
+    sources_text = "\n\n".join(sources_blocks)
 
     response = client.chat.completions.create(
         model=MODEL,
         messages=[
             {"role": "system", "content": RAG_SYSTEM_PROMPT},
-            {"role": "user",   "content": f""" Question: {question} Sources: {sources_text} """
-            }
+            {"role": "user", "content": f""" Question: {question} Sources: {sources_text}"""}
         ],
         temperature=0.0
     )
