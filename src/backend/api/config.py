dc76677b526700f24524bcf511ababf71880e1a8
CyberXpert <nurainiabubakar3537@gmail.com>
2026-02-25 15:46:48 +0100

    should support other AI_CONFIG_DATA

--- src/backend/api/config.py
+++ src/backend/api/config.py
@@ -1,7 +1,14 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL = "llama-3.3-70b-versatile"
+AI_JWT_ISS= os.getenv("AI_JWT_ISS")
+AI_JWT_AUD = os.getenv("AI_JWT_AUD")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+with open(Path("v2/keys/public.pem"), "r") as f:
+    AI_JWT_PUBLIC_KEY = f.read()
