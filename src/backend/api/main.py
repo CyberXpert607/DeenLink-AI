c359ba379068fe2dc52b1a633ae027ae6b7d86b6
CyberXpert <nurainiabubakar3537@gmail.com>
2026-02-25 15:47:39 +0100

    few changes to support dev

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,20 +1,28 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
+from v2.db.database import engine
+from v2.db.models import Base
 from fastapi.middleware.cors import CORSMiddleware
-from v1.api import router as router_v1
 from v2.api import router as router_v2
 
-app = FastAPI(title="DeenLink AI")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(title="DeenLink AI", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], #this is just for dev, of course :0
+    allow_origins=["http://localhost:5500",
+                   "http://127.0.0.1:5500"], 
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=True,
 )
 
-app.include_router(router_v1)
 app.include_router(router_v2)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
